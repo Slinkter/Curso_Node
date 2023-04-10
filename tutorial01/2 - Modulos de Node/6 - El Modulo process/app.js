848cd87c3ab7543a1e0e4f1e1e139705db09ed49
slinkter <luis.j.cueva@gmail.com>
2023-04-10 18:19:00 -0500

    github

--- tutorial01/2 - Modulos de Node/6 - El Modulo process/app.js
+++ tutorial01/2 - Modulos de Node/6 - El Modulo process/app.js
@@ -2,14 +2,19 @@ console.log(process);
 console.log(process.env);
 
 // El indice inicia en 0 como en los arrays.
+console.log(process.argv);
+/* 
 console.log(process.argv[2]);
-console.log(process.argv[3]);
+console.log(process.argv[3]); 
+*/
 
 // Ciclo for para iterar sobre todos los argumentos
 // dados en la línea de comandos.
+/* 
 for (let i = 2; i < process.argv.length; i++) {
   console.log(process.argv[i]);
-}
+} 
+*/
 
 // Informacion sobre el uso de memoria.
 console.log(process.memoryUsage());
