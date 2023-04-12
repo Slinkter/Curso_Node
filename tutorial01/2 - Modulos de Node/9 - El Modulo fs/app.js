f59c7515d8f74a6a987bca7b69f6847ab5e367c3
jhonnycave <luis.j.cueva@gmail.com>
2023-04-11 19:46:25 -0500

    3-paquetes npm

--- tutorial01/2 - Modulos de Node/9 - El Modulo fs/app.js
+++ tutorial01/2 - Modulos de Node/9 - El Modulo fs/app.js
@@ -1,86 +1,84 @@
-/*
-* Curso de Node.js y Express.
-* Creado para freeCodeCamp en Español.
-* Por: Estefania Cassingena Navone. 
-*/
-
-const fs = require('fs');
-
+// Leer
+// Modificar
+// Copiar
+// Eliminar
+// cambiar de nombre
+//
 // -------------------------
 // Versiones asincronas.
 // -------------------------
+const fs = require("fs");
+console.log("Antes de leer el archivo...");
 
-console.log('Antes de leer el archivo...');
-
-// Leer un archivo.
-
-fs.readFile('index.html', 'utf-8', (err, contenido) => {
+//
+// 1.Metodod readFile - Leer un archivo(html)
+fs.readFile("index.html", "utf-8", (err, contenido) => {
   if (err) {
     throw err;
   }
   console.log(contenido);
 });
 
-console.log('Despues de leer el archivo...');
+console.log("Despues de leer el archivo...");
+//
+// 2.Cambiar el nombre de un archivo.
 
-// Cambiar el nombre de un archivo.
-
-fs.rename('index.html', 'main.html', (err) => {
+fs.rename("index.html", "main.html", (err) => {
   if (err) {
     throw err;
   }
-  console.log('Nombre cambiado exitosamente.');
+  console.log("Nombre cambiado exitosamente.");
 });
 
-console.log('Depues de cambiar el nombre del archivo...');
+console.log("Depues de cambiar el nombre del archivo...");
 
-// Agregar contenido al final de un archivo. 
+// Agregar contenido al final de un archivo.
 // Crear un archivo nuevo si no existe.
 
-fs.appendFile('index.html', '<p>Hola</p>', (err) => {
+fs.appendFile("index.html", "<p>Hola</p>", (err) => {
   if (err) {
     throw err;
   }
-  console.log('Archivo actualizado');
+  console.log("Archivo actualizado");
 });
 
-console.log('Depues de agregar contenido al archivo...');
+console.log("Depues de agregar contenido al archivo...");
 
-// Reemplazar contenido del archivo. 
+// Reemplazar contenido del archivo.
 // Crear un archivo nuevo si no existe.
 
-fs.writeFile('index.html', 'Contenido nuevo', (err) => {
+fs.writeFile("index.html", "Contenido nuevo", (err) => {
   if (err) {
     throw err;
   }
-  console.log('Contenido reemplazado exitosamente.');
+  console.log("Contenido reemplazado exitosamente.");
 });
 
-console.log('Depues de reemplazar el contenido del archivo...');
+console.log("Depues de reemplazar el contenido del archivo...");
 
 // Eliminar archivos.
 
-fs.unlink('main.html', (err) => {
+fs.unlink("main.html", (err) => {
   if (err) {
     throw err;
   }
-  console.log('Archivo eliminado');
+  console.log("Archivo eliminado");
 });
 
-console.log('Depues de eliminar un archivo...');
+console.log("Depues de eliminar un archivo...");
 
 // -------------------------
 // Versiones sincronas.
 // -------------------------
 
-const archivo = fs.readFileSync('index.html', 'utf-8');
+const archivo = fs.readFileSync("index.html", "utf-8");
 
 console.log(archivo);
 
-fs.renameSync('index.html', 'main.html');
+fs.renameSync("index.html", "main.html");
 
-fs.appendFileSync('index.html', '<p>Hola</p>');
+fs.appendFileSync("index.html", "<p>Hola</p>");
 
-fs.writeFileSync('index.html', 'Contenido nuevo');
+fs.writeFileSync("index.html", "Contenido nuevo");
 
-fs.unlinkSync('main.html');
+fs.unlinkSync("main.html");
