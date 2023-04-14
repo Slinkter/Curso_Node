d2f03288aebae9a20378899e1fb9bdcde8da60d0
slinkter <luis.j.cueva@gmail.com>
2023-04-14 16:24:06 -0500

    ok

--- tutorial01/5 - Tu Primer Servidor con Node.js y el modulo http/1 - Primer Servidor con Node/app.js
+++ tutorial01/5 - Tu Primer Servidor con Node.js y el modulo http/1 - Primer Servidor con Node/app.js
@@ -1,16 +1,10 @@
-/*
-* Curso de Node.js y Express.
-* Creado para freeCodeCamp en Español.
-* Por: Estefania Cassingena Navone. 
-*/
-
 // Incluir el modulo http.
-const http = require('http');
+const http = require("http");
 
 // Crear un servidor que envie 'Hola, mundo'
 // al cliente que realice la solicitud.
 const servidor = http.createServer((req, res) => {
-  res.end('Hola, mundo');
+  res.end("Hola, mundo");
 });
 
 const puerto = 3000;
@@ -18,5 +12,7 @@ const puerto = 3000;
 // El servidor escucha en el puerto 3000 y cuando
 // comience a escuchar se mostrara este mensaje en el terminal.
 servidor.listen(puerto, () => {
-  console.log(`El servidor esta escuchando en el puerto http://localhost:${puerto}/...`);
+  console.log(
+    `El servidor esta escuchando en el puerto http://localhost:${puerto}/...`
+  );
 });
