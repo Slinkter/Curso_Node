8d5779d16e1e048ad88a55c54e00bc42bd837f39
jhonnycave <luis.j.cueva@gmail.com>
2023-04-25 17:49:46 -0500

    Routing init

--- tutorial01/6 - Modulo url/app.js
+++ tutorial01/6 - Modulo url/app.js
@@ -1,43 +1,33 @@
-/*
-* Curso de Node.js y Express.
-* Creado para freeCodeCamp en Español.
-* Por: Estefania Cassingena Navone. 
-*/
-
 // -------------------------------
-// Extraer informacion de una URL  
+// Extraer informacion de una URL
 // -------------------------------
 
 // Crear un objeto URL a partir de una cadena de caracteres.
-const miUrl = new URL('https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1');
+const miUrl = new URL(
+  "https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1"
+);
 
-// Nombre del host
-console.log(miUrl.hostname);
-
-// Path (camino)
-console.log(miUrl.pathname);
-
-// Protocolo
-console.log(miUrl.protocol);
+console.log(miUrl.hostname); // Nombre del host
+console.log(miUrl.pathname); // Path (camino)
+console.log(miUrl.protocol); // Protocolo
 
 // Parametros Query
 
 // Retorna un objeto URLSearchParams que representa los parámetros
-// query (de búsqueda) de la URL. 
+// query (de búsqueda) de la URL.
 console.log(miUrl.searchParams);
 console.log(typeof miUrl.searchParams);
 
-console.log(miUrl.searchParams.get('ordenarPor'));
-console.log(miUrl.searchParams.get('nivel'));
-
+console.log(miUrl.searchParams.get("ordenarPor"));
+console.log(miUrl.searchParams.get("nivel"));
 
 // ---------------------------
 // Extra: crear una URL
 // ---------------------------
 
-const nuevaURL = new URL('https://www.ejemplo.org');
+const nuevaURL = new URL("https://www.ejemplo.org");
 
-nuevaURL.pathname = '/cursos/programacion';
-nuevaURL.search = '?ordenar=vistas&nivel=1';
+nuevaURL.pathname = "/cursos/programacion";
+nuevaURL.search = "?ordenar=vistas&nivel=1";
 
 console.log(nuevaURL.toString());
